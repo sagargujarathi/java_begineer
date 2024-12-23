4872a204e051a40322c50617b9afeaf2ef208c38
Sagar Gujarathi <sagar.gujarathi@dltapps.co.uk>
2024-12-23 09:51:17 +0530

    First commit

---
+++ src/main/java/com/example/FIRST_SPRINT_PROJECT/FirstSprintProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.FIRST_SPRINT_PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSprintProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSprintProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FIRST_SPRINT_PROJECT/FirstSprintProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FIRST_SPRINT_PROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSprintProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
